43845610a4554ad89cb33b9c4baed9307ff2ece8
6heLL66 <51162582+6heLL66@users.noreply.github.com>
2021-04-06 17:00:22 +0300

    delete comments and adding constants

---
+++ constants/constantValues.js
@@ -0,0 +1,10 @@
+export const dateFactor = 1000
+
+export const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+}
---
+++ constants/secretKeys.js
@@ -0,0 +1,1 @@
+export const apiKey = "33178d46dea4c98a92d98aa6ea4ebc24"
